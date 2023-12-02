4b61fddeaec8fcb50e00eb287d38b6d895ea21c7
EbelSegura <ebel.segura@gmail.com>
2023-12-02 13:49:33 -0600

    Acceso de usuarios

---
+++ Drako-Facturacion/Controllers/ControlsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Drako_Facturacion.Controllers
+{
+    public class ControlsController : Controller
+    {
+        // GET: Controls
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
+    }
+}
---
+++ Drako-Facturacion/Models/ViewModel/ListTablaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Drako_Facturacion.Models.ViewModel
+{
+    public class ListTablaViewModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Passwords { get; set; }
+        public int IdState { get; set; }
+    }
+}
